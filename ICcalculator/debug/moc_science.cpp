090ce4a84f469d6ff83374de874a649e8f21b3e2
tlainfou <494966054@qq.com>
2019-07-14 16:33:16 +0800

    二次修改
    我枯了

--- ICcalculator/debug/moc_science.cpp
+++ ICcalculator/debug/moc_science.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../ICcalculator/science.h"
+#include "../science.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
